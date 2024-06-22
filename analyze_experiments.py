f2e59c4d6f8706afe964d5d1f000fe8ae45104af
AntoDeca1 <antoniodecandia01@gmail.com>
2024-06-22 16:11:33 +0200

    trial

--- analyze_experiments.py
+++ analyze_experiments.py
@@ -3,7 +3,7 @@ Script made to compare a baseline with the corresponding LSH version
 """
 from elliot.utils.lsh_utils import find_best_experiment_static
 
-baseline_path = "results_lsh/comparisons/amazon_music_large/ItemKNN/ItemKNN_baseline.tsv"
-lsh_path = "results_lsh/comparisons/amazon_music_large/ItemKNN/ItemKNN_rp_hashtables.tsv"
+baseline_path = "results_lsh/comparisons/epinions/UserKNN/UserKNN_baseline.tsv"
+lsh_path = "results_lsh/comparisons/epinions/UserKNN/UserKNN_rp_hashtables.tsv"
 
 find_best_experiment_static(baseline_path, lsh_path)
